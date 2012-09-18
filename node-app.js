3069d60ebce35ac7f07f8ef95b4ce4311df3bae3
danm <writetodan@yahoo.com>
2012-09-18 23:53:53 +0100

    node serviing up fabric.js canvas with draggable green card

--- node-app.js
+++ node-app.js
@@ -10,14 +10,28 @@ var describe = function(obj) {
 
 var sys = require('sys');
 var handler = function(request, response) {
-  sys.log('blah'); 
-  sys.log('request: ' + describe(request)); 
-  sys.log('response: ' + describe(response)); 
-  response.writeHeader('Content-Type', 'text/html');
-  response.write('<html><body><div id="message">hello <span class="name">You</span>!</div></body></html>');
-  response.end();
+  sys.log('url: ' + request.url); 
+  if (request.url.indexOf('wall') !== -1) {
+    sys.log('blah'); 
+    sys.log('request: ' + describe(request)); 
+    sys.log('response: ' + describe(response)); 
+    response.writeHeader('Content-Type', 'text/html');
+    response.write('<html><head><script src="lib/fabric.js"></script></head><body><div id="message">hello <span class="name">You</span>!</div><canvas id="wall" width="940" height="500"></canvas><script src="lib/whalley-client.js"></script></body></html>');
+    response.end();
+  }
+  else if (request.url.indexOf('.js') !== -1) {
+    var fs = require('fs');
+    var filedata = fs.readFileSync('.' + request.url, "utf8");
+    response.writeHeader(200, {'Content-Type': 'text/javascript'});
+    response.write(filedata);
+    response.end();
+  }
 };
 
+var connect = require('connect');
 var http = require('http');
+
 var server = http.createServer(handler);
 server.listen(1234);
+
+
