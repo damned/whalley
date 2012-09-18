330031d88614d74e28e42146c10ffcbd580e3261
danm <writetodan@yahoo.com>
2012-09-18 22:41:26 +0100

    copied old node.js server

---
+++ stash.js
@@ -0,0 +1,23 @@
+
+
+var describe = function(obj) {
+  var props = '';
+  for (prop in obj) {
+    props += ' - ' + prop + '\n';
+  }
+  return props;
+}
+
+var sys = require('sys');
+var handler = function(request, response) {
+  sys.log('blah'); 
+  sys.log('request: ' + describe(request)); 
+  sys.log('response: ' + describe(response)); 
+  response.writeHeader('Content-Type', 'text/html');
+  response.write('<html><body><div id="message">hello <span class="name">Aled</span>!</div></body></html>');
+  response.close();
+};
+
+var http = require('http');
+var server = http.createServer(handler);
+server.listen(1234);
