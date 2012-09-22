80f609ba170c566fad9f405197aaa4da86d3c70f
danm <writetodan@yahoo.com>
2012-09-22 20:31:31 +0100

    added password to mql cards fetch from mingle; so that don't checkin password, put in form and made fetch_cards an ajax call

--- node-app.js
+++ node-app.js
@@ -9,45 +9,43 @@ var describe = function(obj) {
 }
 
 var sys = require('sys');
-var card_data = '';
 var https = require('https');
 
-sys.log('making mql request...(using ' + https + ')');
+var username = 'dmoore';
 
-    https.get({ host: "mingle01.thoughtworks.com", path: "/api/v2/projects/is_websites_development/cards/execute_mql.json?mql=select%20name,%20number%20where%20stage%20%3E%20wishlist%20and%20stage%20%3C%20done"}, function(res) {
-      sys.log("Got mql response: " + res.statusCode);
-      res.on('data', function(data) {
-        sys.log("Got data: " + data);
-        card_data = card_data + data;
-      });
-      res.on('end', function() {
-        sys.log("Got all card data: " + card_data);
-      });
-    }).on('error', function(error) {
-      sys.log(error);
+var fetch_cards = function(password, on_success, on_error) {
+  sys.log('making mql request...(using ' + https + ')');
+
+  var cards = '';
+
+  var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+  var header = {'Host': 'mingle01.thoughtworks.com', 'Authorization': auth};
+
+  https.get({ host: "mingle01.thoughtworks.com", path: "/api/v2/projects/is_websites_development/cards/execute_mql.json?mql=select%20name,%20number%20where%20stage%20%3E%20wishlist%20and%20stage%20%3C%20done", headers: header }, function(res) {
+    sys.log("Got mql response: " + res.statusCode);
+    res.on('data', function(data) {
+      sys.log("Got data: " + data);
+      cards = cards + data;
     });
+    res.on('end', function() {
+      sys.log("Got all card data: " + cards);
+      on_success(cards);
+    });
+  }).on('error', function(error) {
+    sys.log(error);
+    on_error(error);
+  });
+};
+
+
+var card_data = '';
+var url_parser = require('url');
+
 
 var handler = function(request, response) {
   sys.log('url: ' + request.url); 
   if (request.url.indexOf('wall') !== -1) {
-  
-  
-    sys.log('blah'); 
-    sys.log('request: ' + describe(request)); 
-    sys.log('response: ' + describe(response)); 
-    response.writeHeader('Content-Type', 'text/html');
-    response.write('<html><head>' 
-    + '<script src="lib/fabric.js"></script>'
-    + '</head><body>' 
-    +   '<div id="message">hello <span class="name">You</span>!</div>'
-    +   '<canvas id="wall" width="940" height="500"></canvas>' 
-    +   '<script src="lib/whalley-client.js"></script>'
-    +   '<script type="text/javascript">'
-    +     'var card_data = ' + card_data + ";"
-    +     'draw_cards(card_data);'
-    +   '</script>'
-    + '</body></html>');
-    response.end();
+    render_wall_to(response);  
   }
   else if (request.url.indexOf('.js') !== -1) {
     var fs = require('fs');
@@ -56,9 +54,48 @@ var handler = function(request, response) {
     response.write(filedata);
     response.end();
   }
+  else if (request.url.indexOf('fetch_cards') !== -1) {
+    var parsed = url_parser.parse(request.url, true);
+    sys.log('parsed: ' + describe(parsed));
+    var password = parsed['query']['password'];
+    fetch_cards(password, function(cards) {
+      render_json_to(cards, response);
+    },
+    function(error) {
+      render_json_to('[ { Name: "' + error + '", Number: "999"} ]', response);
+    });
+  }
 };
 
-var connect = require('connect');
+function render(type, content, response) {
+    response.writeHeader('Content-Type', type);
+    response.write(content);
+    response.end();
+};
+
+function render_json_to(json, response) {
+    render('application/json', json, response);
+};
+
+function render_wall_to(response) {
+    render('text/html', 
+      '<html><head>' 
+      + '<script src="lib/fabric.js"></script>'
+      + '<script src="lib/jquery-1.8.2.js"></script>'
+      + '<script type="text/javascript">'
+      + "  var fetch_cards = function() { var card_data = $.getJSON('fetch_cards?password=' + $('#password').val(), function(card_data) {"
+      + '      draw_cards(card_data);'
+      + '    });'
+      + '  };'
+      + '</script>'
+      + '</head><body>' 
+      +   '<div id="message">hello <span class="name">' + username + '</span>! Please enter password and hit go <input type="password" id="password"/><input type="button" value="Go" onclick="fetch_cards();"/></div>'
+      +   '<canvas id="wall" width="940" height="500"></canvas>' 
+      + '  <script src="lib/whalley-client.js"></script>'
+      + '</body></html>',
+      response);
+};
+
 var http = require('http');
 
 var server = http.createServer(handler);
