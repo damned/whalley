b4f285c5684f915ab6116c42c438eafff45adca8
danm <writetodan@yahoo.com>
2012-10-03 18:30:06 +0100

    prevent static file requests using '..'

--- static-files.js
+++ static-files.js
@@ -2,7 +2,11 @@ var resources = require('./resources');
 var fs = require('fs');
 
 var respond_with_file = function(url, content_type, response) {
-  // TODO securify
+  if (url.indexOf('..') != -1) {
+    response.statusCode = 403;
+    response.end;
+    return;
+  }
   var filedata = fs.readFileSync('.' + url);
   resources.render(content_type, filedata, response);
 }
