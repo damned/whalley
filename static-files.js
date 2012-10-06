e4c26dce7cff642bdd2dccf10332fa10dfe61943
danm <writetodan@yahoo.com>
2012-10-06 16:47:14 +0100

    hacked in a basic image card

--- static-files.js
+++ static-files.js
@@ -7,12 +7,21 @@ var respond_with_file = function(url, content_type, response) {
     response.end;
     return;
   }
-  var filedata = fs.readFileSync('.' + url);
-  resources.render(content_type, filedata, response);
+  try {
+    var filedata = fs.readFileSync('.' + url);
+    resources.render(content_type, filedata, response);
+  }
+  catch(e) {
+    response.statusCode = 404;
+    response.end;
+  }
 }
 
 resources.add_on_url_contains('.jpg', function(request, response) {
   respond_with_file(request.url, 'image/jpeg', response)
+});
+resources.add_on_url_contains('.png', function(request, response) {
+  respond_with_file(request.url, 'image/png', response)
 });
 resources.add_on_url_contains('.js', function(request, response) {
   respond_with_file(request.url, 'text/javascript', response);
