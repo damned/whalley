468c51ffc1c4db3a2ea114fa9b29d838fdd78aa9
danm <writetodan@yahoo.com>
2013-10-06 16:45:46 +0100

    @damned hacked video capture into working on firefox - doesn't seem to work on chrome

--- test/client/image-capture-tests.js
+++ test/client/image-capture-tests.js
@@ -1,8 +1,25 @@
 var expect = chai.expect;
 
 describe('whalley image capture', function() {
+  beforeEach(function() {
+    $('#sandbox').empty();
+  })
+
   it('should be an object', function() {
     var view = {};
     expect(whalley.image_capture(view)).to.exist;
+  })
+})
+
+describe('whalley image capture view', function() {
+  it('should place a video and canvas in image capture div', function() {
+    $('#sandbox').append('<div id="image-capture"/>');
+
+    var capture_html = $('#image-capture');
+
+    whalley.image_capture_view()
+
+    expect(capture_html.find('video').length).to.equal(1);
+    expect(capture_html.find('canvas').length).to.equal(1);
   })
 })
