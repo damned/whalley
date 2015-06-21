7cb55f3a96a205275dd2703b0911c12cd73087ec
danm <writetodan@yahoo.com>
2015-06-21 11:23:32 +0100

    @damned convert v0.1 to v0.1; refactored tests

--- test/server/wall_data_converter_tests.js
+++ test/server/wall_data_converter_tests.js
@@ -6,44 +6,49 @@ var converter = require('../../lib/server/wall_data_converter');
 
 describe('wall_data_converter', function() {
   var target_version;
-  describe('converting to pre-versioning data format', function() {
-    it('leaves unversioned data unversioned', function() {
-      var data = {
-        'look_no_data_version_property': 'because i was written before versioning',
+  describe('converting between 0.1 and pre-versioning formats', function() {
+    var pre_versioning_format = {
+      meta: {
+        'lowfi-card-ids': [
+          "card-a",
+          "card-b"
+        ]
+      },
+      cards: {
+        'card-a': {id: 'card-a', top: 123},
+        'card-b': {id: 'card-b', top: 456}
       }
-      expect(converter.convert(data, null)).to.equal(data);
+    };
+
+    var version_0_1_format = {
+      data_version: '0.1',
+      cards: [
+        { id: 'card-a', top: 123 },
+        { id: 'card-b', top: 456 }
+      ]
+    }
+
+    it('leaves data when converting preversioned -> preversioned', function() {
+      expect(converter.convert(pre_versioning_format, null)).to.equal(pre_versioning_format);
+    });
+    it('leaves data when converting 0.1 -> 0.1', function() {
+      var converted = converter.convert(version_0_1_format, '0.1');
+
+      expect(converted).to.equal(version_0_1_format);
     });
 
     it('converts version 0.1 data, removing the data version', function() {
-      var converted = converter.convert({data_version: '0.1', cards: []}, null);
+      var converted = converter.convert(version_0_1_format, null);
 
       expect(converted.data_version).to.not.exist;
     });
 
     it('converts version 0.1 cards to pre-versioning format', function() {
-      var data = {
-        data_version: '0.1',
-        cards: [
-          { id: 'card-a', top: 123 },
-          { id: 'card-b', top: 456 }
-        ]
-      }
+      var converted = converter.convert(version_0_1_format, null);
 
-      var converted = converter.convert(data, null);
-
-      json(converted).should.equal(json({
-        meta: {
-          'lowfi-card-ids': [
-              "card-a",
-              "card-b"
-          ]
-        },
-        cards: {
-          'card-a': { id: 'card-a', top: 123 },
-          'card-b': { id: 'card-b', top: 456 }
-        }
-      }));
+      json(converted).should.equal(json(pre_versioning_format));
     });
+
     function json(obj) {
       return JSON.stringify(obj);
     }
