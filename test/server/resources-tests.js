e3bf8b48ca5324c0231235cd1a1bbd5db413f5ac
danm <writetodan@yahoo.com>
2015-06-22 09:36:54 +0100

    @damned moved saving of wall data to server into sync client
    removed autosave
    using structure_version 0.1 for all line calls (still some pre-versioning data stored maybe)

--- test/server/resources-tests.js
+++ test/server/resources-tests.js
@@ -6,25 +6,27 @@ var resources = require('../../lib/server/resources');
 var response = {};
 var match_handler;
 var result, url;
+var method;
 
 describe('resources', function() {
-  describe('GetUrlStartsWithHandler', function() {
+  describe('MethodUrlStartsWithHandler', function() {
     beforeEach(function() {
       match_handler = function() {
         match_handler.called = true;
       };
       match_handler.called = false;
-      handler = new resources.GetUrlStartsWithHandler('/the_prefix', match_handler);
+      method = 'GET';
+      handler = new resources.MethodUrlStartsWithHandler(method, '/the_prefix', match_handler);
     });
     context('url starts with prefix', function() {
       beforeEach(function() {
         url = '/the_prefix/the_rest_of_url';
       });
-      context('is a GET request', function() {
+      context('is of the specified request method', function() {
         beforeEach(function() {
           result = handler.handle({
             url: url,
-            method: 'GET'
+            method: method
           }, response)
         });
         it('returns true', function() {
@@ -34,11 +36,11 @@ describe('resources', function() {
           expect(match_handler.called).to.be.true
         });
       })
-      context('is not a GET request', function() {
+      context('is not the specified request method', function() {
         beforeEach(function() {
           result = handler.handle({
             url: url,
-            method: 'POST'
+            method: 'NOT' + method
           }, response)
         });
         it('returns false', function() {
@@ -52,7 +54,8 @@ describe('resources', function() {
     context('url does not start with prefix', function() {
       beforeEach(function() {
         result = handler.handle({
-          url: '/not_the_prefix/the_rest_of_url'
+          url: '/not_the_prefix/the_rest_of_url',
+          method: method
         }, response)
       });
 
