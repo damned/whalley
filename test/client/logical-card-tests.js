64caea1a6fe22348407f59f0444c1ee398870853
danm <writetodan@yahoo.com>
2015-07-31 20:07:05 +0100

    @damned forwarding svg card data() to logical

--- test/client/logical-card-tests.js
+++ test/client/logical-card-tests.js
@@ -43,6 +43,75 @@ describe('logical card', function() {
     })
   })
 
+  describe('disallowed property names', function() {
+    it('cannot have a data field as it conflicts with the data() serialisation method', function() {
+      expect(function() {
+        a_card({ data: 'some data' })
+      }).to.throw(/data/)
+    })
+  })
+
+  describe('data()', function() {
+    it('returns object including all defaulted properties', function() {
+      data = a_card({}).data()
+
+      expect(data.type).to.be.a('string')
+      expect(data.left).to.be.a('number')
+      expect(data.top).to.be.a('number')
+      expect(data.width).to.be.a('number')
+      expect(data.height).to.be.a('number')
+    })
+    it('returns an object including any specified un-defaulted properties', function() {
+      card = a_card({ 'colour': 'red' })
+
+      expect(card.data)
+    })
+  })
+
+  describe('last_modification_status', function() {
+    context('when card is moved', function(){
+      it('is set to the value of status', function() {
+        card = a_card({
+          last_modification_status: 'old_status',
+          status: 'current_status'
+        })
+
+        card.move_to(0, 0)
+
+        expect(card.last_modification_status).to.eq('current_status')
+      })
+      it('is removed when there is no current status', function() {
+        card = a_card({
+          last_modification_status: 'old_status',
+        })
+
+        card.move_to(0, 0)
+
+        expect(card.last_modification_status).to.be.undefined
+      })
+      it('is removed when status is null', function() {
+        card = a_card({
+          last_modification_status: 'old_status',
+          status: null // TODO force null status -> undefined and simplify this behaviour instead
+        })
+
+        card.move_to(0, 0)
+
+        expect(card.last_modification_status).to.be.undefined
+      })
+      it('is removed when status is empty', function() {
+        card = a_card({
+          last_modification_status: 'old_status',
+          status: '' // TODO force empty status -> undefined and simplify this behaviour instead
+        })
+
+        card.move_to(0, 0)
+
+        expect(card.last_modification_status).to.be.undefined
+      })
+    })
+  })
+
   describe('arbitrary properties', function() {
     it('reflects an arbitrary initial property', function() {
       card = a_card({ some_property: 'abc'})
