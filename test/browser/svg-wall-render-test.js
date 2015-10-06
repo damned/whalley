5a735728a162fdd3a000808940511424eb8e5691
danm <writetodan@yahoo.com>
2015-10-06 08:48:16 +0100

    @damned  wrap webdriver - using es6 class oooh

--- test/browser/svg-wall-render-test.js
+++ test/browser/svg-wall-render-test.js
@@ -1,27 +1,60 @@
+'use strict';
+
 require('should')
 var chai = require('chai')
 var expect = chai.expect
 chai.use(require('chai-as-promised'))
 var webdriver = require('selenium-webdriver')
 
+class Page {
+  constructor(browser) {
+    this.browser = browser
+  }
+
+  title() {
+    return this.browser.title()
+  }
+}
+
+class Browser {
+  constructor() {
+    this.base_uri = 'http://localhost:1234'
+    this.driver = new webdriver.Builder()
+        .withCapabilities(webdriver.Capabilities.chrome())
+        .build();
+  }
+  start() {
+    return this.driver.getWindowHandle()
+  }
+  open(path) {
+    this.driver.get(this.base_uri + path)
+    return new Page(this)
+  }
+  title() {
+    return this.driver.getTitle()
+  }
+  quit() {
+    this.driver.quit()
+  }
+}
+
 describe('svg wall rendering', function() {
-  var driver
+  var browser
   this.timeout(0)
 
   before(function() {
-    driver = new webdriver.Builder()
-        .withCapabilities(webdriver.Capabilities.chrome())
-        .build();
+    browser = new Browser()
 
-    return driver.getWindowHandle()
+    return browser.start()
   })
   after(function() {
-    return driver.quit()
+    return browser.quit()
   })
 
-  it('displays a card', function(done) {
-    driver.get('http://localhost:1234/walls/json')
 
-    expect(driver.getTitle()).to.eventually.equal('Whalley SVG Card Wall').notify(done)
+  it('displays a card', function(done) {
+    let page = browser.open('/walls/json')
+
+    expect(page.title()).to.eventually.equal('Whalley SVG Card Wall').notify(done)
   })
 })
