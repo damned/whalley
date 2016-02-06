e2585306c89d81d92863160aa2d71cd55ad0aab6
danm <writetodan@yahoo.com>
2016-02-06 15:26:02 +0000

    @damned  find specific card
    ah no - bug in debugging around isPromise!!

--- test/browser/browser.js
+++ test/browser/browser.js
@@ -4,22 +4,66 @@ var webdriver = require('selenium-webdriver')
 class Node {
   constructor(element) {
     this.element = element
+    this.type = 'node'
   }
   find(locator, extras) {
     let options = extras || {as: Node}
     let type = options.as
-    return new type(this.element.findElement(({css: locator})))
+    return new type(this.element.findElement({css: locator}))
+  }
+  all(locator, extras) {
+    let options = extras || {as: Node}
+    let type = options.as
+    let elements = this.element.findElements({css: locator})
+    check(elements, 'all elements')
+    // NB findElements returns promise of an array
+    return new type(elements)
   }
   get text() {
-    return this.element.getText()
+    check(this.element, this.type + ' this.element')
+
+    var d = webdriver.promise.defer();
+    this.element.then(function(el) {
+      d.fulfill(el.getText());
+    })
+    return d.promise;
   }
-  then(success, failure) {
-    return this.element.then(success, failure)
+}
+
+class Nodes {
+  constructor(promise_of_elements) {
+    this.promise_of_elements = promise_of_elements
+    this.child_type = Node
+  }
+  find_by_text(name) {
+    check(this.promise_of_elements, "promise_of_elements")
+    let d = webdriver.promise.defer();
+    let promise_of_first_element = this.promise_of_elements.then(function(all_found) {
+      console.log('actually all_found: ' + all_found.length)
+      all_found.forEach(function(to_filter) {
+        to_filter.getText().then(function(text) {
+          if (text.startsWith(name)) {
+            d.fulfill(to_filter)
+          }
+        })
+      });
+    })
+    return new this.child_type(d.promise);
   }
 }
 
 class Card extends Node {
+  constructor(element) {
+    super(element)
+    this.type = 'card'
+  }
+}
 
+class Cards extends Nodes {
+  constructor(elements) {
+    super(elements)
+    this.child_type = Card
+  }
 }
 
 class Wall {
@@ -29,6 +73,11 @@ class Wall {
 
   get card() {
     return this.node.find('g.card', {as: Card})
+  }
+
+  card_named(name) {
+    var cards = this.node.all('g.card', {as: Cards});
+    return cards.find_by_text(name);
   }
 }
 
@@ -46,6 +95,14 @@ class Page {
   }
 }
 
+function check(object, name) {
+  console.log(name + ' raw: '    + object)
+  console.log(name + ': '    + Object.prototype.toString(object))
+  console.log(name + ' length: '    + object.length)
+  console.log(name + ' then: '    + object.then)
+  console.log(name + ' findElement: '    + object.findElement)
+  console.log(name + ' is promise: '    + webdriver.promise.isPromise(object))
+}
 
 class Browser {
   constructor() {
--- test/browser/svg-wall-render-test.js
+++ test/browser/svg-wall-render-test.js
@@ -7,24 +7,45 @@ chai.use(require('chai-as-promised'))
 var Browser = require('./browser')
 
 
+var webdriver = require('selenium-webdriver')
+
+
+
 describe('svg wall rendering', function() {
-  var browser
+  var browser, page
   this.timeout(0)
 
-  before(function() {
+  before(() => {
     browser = new Browser()
 
     return browser.start()
   })
-  after(function() {
+
+  before(() => {
+    page = browser.open('/walls/json')
+  })
+
+
+  after(() => {
     return browser.quit()
   })
 
 
-  it('displays a card', function(done) {
-    let page = browser.open('/walls/json')
-
+  it('displays a card', (done) => {
     expect(page.title()).to.eventually.equal('Whalley SVG Card Wall')
     expect(page.wall().card.text).to.eventually.equal('MINGLE').notify(done)
   })
+
+  it('has other cards', (done) => {
+    expect(page.wall().card_named('TODO').text).to.eventually.equal('TODO').notify(done)
+  })
 })
+
+function check(object, name) {
+  console.log(name + ' raw: '    + object)
+  console.log(name + ': '    + Object.prototype.toString(object))
+  console.log(name + ' length: '    + object.length)
+  console.log(name + ' then: '    + object.then)
+  console.log(name + ' findElement: '    + object.findElement)
+  console.log(name + ' is promise: '    + webdriver.promise.isPromise(object))
+}
