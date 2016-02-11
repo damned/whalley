b0825bfac2ae59c4117508261eb166165c57db4d
danm <writetodan@yahoo.com>
2016-02-11 02:41:05 +0000

    @damned run app from test process

--- build.js
+++ build.js
@@ -5,13 +5,30 @@ echo('Building...')
 
 var child = require('child_process')
 
-var to_stdout = function(error, out, err) {
-  sys.puts(out)
-  if (err) sys.puts(err)
-  if (error) sys.puts("error: " + error)
+var to_stdout = function(after) {
+  return function(error, out, err) {
+    sys.puts(out)
+    if (err) sys.puts(err)
+    if (error) sys.puts("error: " + error)
+    after()
+  }
 }
 
-child.exec('mocha test/server', to_stdout)
-child.exec('mocha test/browser', to_stdout)
+var app = child.spawn('nodejs', ['whalley-node-app.js'])
+app.on('error', (err) => {
+  console.log('Error running app process while testing: ' + err);
+});
 
-echo('Done.')
+child.exec('mocha test/server', to_stdout(function() {
+  child.exec('mocha test/browser', to_stdout(function() {
+    echo('Tests complete')
+    process.exit(0)
+  }))
+}))
+
+process.on('exit', function() {
+  echo('Exiting, attempting to kill app, pid: ' + app.pid)
+  app.stdin.end()
+  app.kill();
+  echo('Done')
+});
